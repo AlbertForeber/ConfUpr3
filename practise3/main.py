a85353f7782ae60eb48ef4155d927ced50c0da0d
albert <albert.foreber@gmail.com>
2025-11-30 22:03:15 +0300

    fifth stage CLI

--- practise3/main.py
+++ practise3/main.py
@@ -4,15 +4,17 @@ from assembler import Assembler
 from practise3.interpreter import Interpreter
 
 if __name__ == "__main__":
-    assembler = Assembler()
-    assembler.test()
-    program = dict()
+    if __name__ == "__main__":
+        assembler = Assembler()
+        program = dict()
 
+        print("Введите путь к файлу с исходным кодом\n>> ", end='')
+        read_from = input()
+        print("Введите путь для файла с машинным кодом\n>> ", end='')
+        write_to = input()
+        assembler.assemble_program(read_from, write_to)
 
-
-    a = assembler.assemble_program("test.csv", "byte_code", True)
-
-    interpreter = Interpreter()
-    interpreter.execute("byte_code")
+        interpreter = Interpreter()
+        interpreter.execute(write_to)
 
 
